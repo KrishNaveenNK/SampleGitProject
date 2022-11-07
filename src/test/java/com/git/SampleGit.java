8bca9fa5da9a46802c938debcb72c2c848fc991c
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 16:55:16 +0530

    methods added

--- src/test/java/com/git/SampleGit.java
+++ src/test/java/com/git/SampleGit.java
@@ -11,4 +11,13 @@ System.out.println("Student2");
 	public void method3() {
 System.out.println("Student3");
 	}
+	public void method4() {
+System.out.println("Student1");
+	}
+	public void method5() {
+System.out.println("Student2");
+	}
+	public void method6() {
+System.out.println("Student3");
+	}
 }
