6161083a2d1ea896cbdbe9dc0cb0f6054e9414ef
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 16:20:39 +0530

    First time push

---
+++ src/test/java/com/git/SampleGit.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class SampleGit {
+
+	public void method1() {
+System.out.println("Student1");
+	}
+	public void method2() {
+System.out.println("Student2");
+	}
+	public void method3() {
+System.out.println("Student3");
+	}
+}
